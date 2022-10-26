ae8c73b7b18b81abb8a5d2880cd6aee9ef60d99e
adantasdf <P_919364@redecamara.camara.gov.br>
2022-10-26 15:41:03 -0300

    atualizando

--- main.js
+++ main.js
@@ -5,23 +5,19 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-
 //enquanto
-while (contador < listaDeTeclas.length) {
-    const tecla = listaDeTeclas(contador);
-    const instrumento = listaDeTeclas[contador].classList[1];
+for (let contador = 0; contador  < listaDeTeclas.length; contador++)  {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
-    console.log(instrumento);
-
-    
-    listaDeTeclas[contador].onclick = function() {
-        tocaSom('#som_tecla_pom')
+    tecla.onclick = function () {
+        tocaSom('#som_tecla_pom');
     }
-    
-
-    contador = contador + 1;
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom(idAudio)
+    }
 
     console.log(contador);
 }
+
